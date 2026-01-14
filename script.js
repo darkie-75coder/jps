728c02ff97221756d1676ed11262e814376a4f1a
darkie-75coder <shivanshkumargupta75@gmail.com>
2026-01-14 15:11:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const sidebar = document.querySelector(".sidebar")
+
+function showsidebar() {
+    sidebar.style.display = "block";
+}
+
+function hidebar() {
+    sidebar.style.display = "none";
+}
